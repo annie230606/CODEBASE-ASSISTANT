e688af6764422a6d690b37225a04bae5d42798c6
annie230606 <annieroy.mulloor@gmail.com>
2025-10-18 10:25:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,29 +11,19 @@ os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT" )
 
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","you are an expert at codebase assisting. Please explain any doubts user has about the input"),
+        ("system","you are a greeter to the user.give polite response to the users input"),
         ("user", "{question}")
     ]
 )
 st.title("codebase Assistant")
 question=st.text_input("What is your Question?")
-tab1, tab2, tab3 = st.tabs(["❓ Ask Question", "📋 Paste Code", "📂 Upload File"])
+tab1 = st.tabs(["Ask Question"])
 
-llm=Ollama(model="gemma3:1b")
+llm=Ollama(model="llama3")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
 if tab1:
-    question = st.text_input("Ask a programming question:")
+    question = st.text_input("Ask a question:")
     if question:
         st.write(chain.invoke({"question": question}))
-if tab2:
-    code=st.text_area("paste your code here")
-    if code:
-        query = f"Explain this code and suggest improvements:\n{code}"
-        st.write(chain.invoke({"question": query}))
-if tab3:
-    file=st.file_uploader("Upload the required file")
-    if file:
-        query=f"Analyze this code file and explain what it does. Suggest improvements if possible:\n{file}"
-        st.write(chain.invoke({"question": query}))
